be28acd9582a0e2cfa579e53c900c7270b31ee84
Neil Sood <soodorama@gmail.com>
2019-05-02 02:13:35 -0700

    call,text,email working

--- AboutMe/ContactVC.swift
+++ AboutMe/ContactVC.swift
@@ -7,25 +7,88 @@
 //
 
 import UIKit
+import MessageUI
+
 
 class ContactVC: UIViewController {
 
     
-    @IBOutlet weak var PhoneButton: UIButton!
-    @IBOutlet weak var EmailButton: UIButton!
-    
+    @IBOutlet weak var phoneButton: UIButton!
+    @IBOutlet weak var emailButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        phoneButton.layer.cornerRadius = 20
+        emailButton.layer.cornerRadius = 20
+    }
+    
+    @objc func handleGesture(gesture: UISwipeGestureRecognizer) -> Void {
+        if gesture.direction == UISwipeGestureRecognizer.Direction.down {
+            print("Swipe Down")
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func phonePressed(_ sender: UIButton) {
+        
+//        let optionMenu = UIAlertController(title: nil, message: "Contact Neil", preferredStyle: .actionSheet)
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Call", style: .default , handler:{ (UIAlertAction)in
+            print("User click Approve button")
+            if let url = URL(string: "tel:16502072277"), UIApplication.shared.canOpenURL(url) {
+                if #available(iOS 10, *) {
+                    print("hi")
+                    UIApplication.shared.open(url)
+                } else {
+                    print("by")
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Text", style: .default , handler:{ (UIAlertAction)in
+            print("User click Edit button")
+            if let url = URL(string: "sms:16502072277"), UIApplication.shared.canOpenURL(url) {
+                if #available(iOS 10, *) {
+                    UIApplication.shared.open(url)
+                } else {
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler:{ (UIAlertAction)in
+            print("User click Dismiss button")
+        }))
+        
+        self.present(alert, animated: true, completion: {
+            print("completion block")
+        })
 
-        // Do any additional setup after loading the view.
+
+        
     }
     
-    @IBAction func PhonePressed(_ sender: UIButton) {
+    @IBAction func emailPressed(_ sender: UIButton) {
+        let email = "neilsood@umich.edu"
+        if let url = URL(string: "mailto:\(email)") {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
     }
-    
-    @IBAction func EmailPressed(_ sender: UIButton) {
-    }
+
     
 }
--- AboutMe/MainVC.swift
+++ AboutMe/MainVC.swift
@@ -8,19 +8,80 @@
 
 import UIKit
 
-class MainVC: UIViewController {
 
-    
-    @IBOutlet weak var ViewWorkButton: UIButton!
-    
+
+class MainVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
-    @IBAction func ViewWorkPressed(_ sender: UIButton) {
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
         
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
     }
     
-}
+    
+    @objc func handleGesture(gesture: UISwipeGestureRecognizer) -> Void {
+        if gesture.direction == UISwipeGestureRecognizer.Direction.right {
+            print("Swipe Right")
+        }
+        else if gesture.direction == UISwipeGestureRecognizer.Direction.left {
+            print("Swipe Left")
+            performSegue(withIdentifier: "ToMichiganSegue", sender: self)
+        }
+        else if gesture.direction == UISwipeGestureRecognizer.Direction.up {
+            print("Swipe Up")
+            performSegue(withIdentifier: "ToContactSegue", sender: self)
+        }
+        else if gesture.direction == UISwipeGestureRecognizer.Direction.down {
+            print("Swipe Down")
+        }
+    }
 
+}
+//
+//extension MainVC: ContactVCDelegate {
+//    func phonePressed() {
+//        print("uihh")
+//        if let url = URL(string: "tel:16502072277"), UIApplication.shared.canOpenURL(url) {
+//            if #available(iOS 10, *) {
+//                print("hi")
+//                UIApplication.shared.open(url)
+//            } else {
+//                print("by")
+//                UIApplication.shared.openURL(url)
+//            }
+//        }
+//        if let url = URL(string: "sms:16502072277"), UIApplication.shared.canOpenURL(url) {
+//            if #available(iOS 10, *) {
+//                UIApplication.shared.open(url)
+//            } else {
+//                UIApplication.shared.openURL(url)
+//            }
+//        }
+//    }
+//    
+//    func emailPressed() {
+//        let email = "neilsood@umich.edu"
+//        if let url = URL(string: "mailto:\(email)") {
+//            if #available(iOS 10.0, *) {
+//                UIApplication.shared.open(url)
+//            } else {
+//                UIApplication.shared.openURL(url)
+//            }
+//        }
+//    }
+//}
+//
